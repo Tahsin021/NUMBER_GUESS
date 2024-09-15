bccf95319267ab6db53c6277a8635d70cbe8a2f6
Tahsin Haider <chtahsinhaider@gmail.com>
2024-09-15 07:45:48 +0000

    chore: commit

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+echo "Enter your username:"
+read NAME
+
+ID=$($PSQL "SELECT player_id FROM players WHERE name=$NAME")
+
+if [[ -z $ID ]]
+then
+  echo "Welcome, $NAME! It looks like this is your first time here."
+  INSERT_NAME=$($PSQL "INSERT INTO players(name) VALUES($NAME)")
+  ID=$($PSQL "SELECT player_id FROM players WHERE name=$NAME")
+fi
+
